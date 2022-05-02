bef6b6fe3b73c00766022bd6d49f62ef13786772
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-02 23:48:55 +0300

    Add labs

---
+++ Labs/Lab.03.P-thBit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab._03.P_thBit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            n >>= p;
+            n &= 1;
+            Console.WriteLine(n);
+        }
+    }
+}
---
+++ Labs/TryParse/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string dataType = " ";
+            int n;
+            float f;
+            char ch;
+            bool b;
+
+            while (input != "END")
+            {
+                if (int.TryParse(input,out n))
+                {
+                    dataType = "integer";
+                    Console.WriteLine($"{input} is {dataType} type");
+                }
+                else if (float.TryParse(input, out f))
+                {
+                    dataType = "floating point";
+                    Console.WriteLine($"{input} is {dataType} type");
+                }
+                else if (char.TryParse(input, out ch))
+                {
+                    dataType = "character";
+                    Console.WriteLine($"{input} is {dataType} type");
+                }
+                else if (bool.TryParse(input, out b))
+                {
+                    dataType = "boolean";
+                    Console.WriteLine($"{input} is {dataType} type");
+                }
+                else
+                {
+                    dataType = "string";
+                    Console.WriteLine($"{input} is {dataType} type");
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
