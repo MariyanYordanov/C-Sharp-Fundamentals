a1b48279788a9b2b133a7e369b8ac1cdf3124c32
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-04 10:33:17 +0300

    Add magic sum

---
+++ 04.Array/ExerciceArray/8. Magic Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] readenArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < readenArray.Length; i++)
+            {
+                for (int j = i + 1; j < readenArray.Length; j++)
+                {
+                    int sum = readenArray[i] + readenArray[j];
+                    if (sum == number)
+                    {
+                        Console.WriteLine($"{readenArray[i]} {readenArray[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
