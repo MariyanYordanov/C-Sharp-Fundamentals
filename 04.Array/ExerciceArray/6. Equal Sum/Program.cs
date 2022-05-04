578652f31ca04199fb7b08b1410615cd48e280c2
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-04 10:29:50 +0300

    Add Equal sum

---
+++ 04.Array/ExerciceArray/6. Equal Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Equal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] readenArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < readenArray.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    leftSum += readenArray[j];
+                }
+                
+                for (int k = i + 1; k < readenArray.Length; k++)
+                {
+                    rightSum += readenArray[k];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
