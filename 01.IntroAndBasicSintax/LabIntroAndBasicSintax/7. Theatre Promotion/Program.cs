7d78acbd9d58c99352126c4885fb98cdcafc9fb8
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-06 00:29:09 +0300

    Add theater promotion

---
+++ 01.IntroAndBasicSintax/LabIntroAndBasicSintax/7. Theatre Promotion/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _07._Theatre_Promotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfDay = Console.ReadLine().ToLower();
+            int personAge = int.Parse(Console.ReadLine());
+            double price = 0;
+
+            if (typeOfDay == "weekday")
+            {
+                if ((personAge >= 0 && personAge <= 18) || (personAge > 64 && personAge <= 122))
+                {
+                    price += 12;
+                }
+
+                else if (personAge > 18 && personAge <= 64)
+                {
+                    price += 18;
+                }
+            }
+
+            else if (typeOfDay == "weekend")
+            {
+                if ((personAge >= 0 && personAge <= 18) || (personAge > 64 & personAge <= 122))
+                {
+                    price += 15;
+                }
+
+                else if (personAge > 18 && personAge <= 64)
+                {
+                    price += 20;
+                }
+            }
+
+            else if (typeOfDay == "holiday")
+            {
+                if (personAge >= 0 && personAge <= 18)
+                {
+                    price += 5;
+                }
+
+                else if (personAge > 18 && personAge <= 64)
+                {
+                    price += 12;
+                }
+
+                else if (personAge > 64 && personAge <= 122)
+                {
+                    price += 10;
+                }
+            }
+
+            if (price != 0)
+            {
+                Console.WriteLine($"{price}$");
+            }
+
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
