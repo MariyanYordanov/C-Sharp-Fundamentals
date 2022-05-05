1d119340b3fb8def6ede6dc0bb05458e3d638f3d
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-06 00:23:12 +0300

    Add Passed or failed

---
+++ 01.IntroAndBasicSintax/LabIntroAndBasicSintax/3.PassedOrFailed/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03.PassedOrFailed.Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            if (number >= 3)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+            
+        }
+    }
+}
