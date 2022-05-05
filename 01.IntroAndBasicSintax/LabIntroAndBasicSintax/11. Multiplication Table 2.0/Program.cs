1f50796828903751098a0d75dd391871b853680f
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-06 00:33:00 +0300

    Add Multiplication table

---
+++ 01.IntroAndBasicSintax/LabIntroAndBasicSintax/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _11._Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int theInteger = int.Parse(Console.ReadLine());
+            int times = int.Parse(Console.ReadLine());
+
+            do
+            {
+                int product = times * theInteger;
+                Console.WriteLine($"{theInteger} X {times} = {product}");
+                times += 1;
+            } while (times <= 10);
+        }
+    }
+}
