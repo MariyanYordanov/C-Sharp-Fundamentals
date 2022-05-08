9e95af0d6c38ee3330c959c9f9adb3f394f825c8
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-08 22:54:30 +0300

    Add Strong number

---
+++ 01.IntroAndBasicSintax/ExerciseIntroAndBasicSintax/6. Strong number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06._Strong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int number = num;
+            int sumFactorial = 0;
+
+            while (number > 0)
+            {
+                int currentNumber = number % 10;
+                number /= 10;
+                int currentFactorialNumber = 1;
+
+                for (int i = 1; i <= currentNumber; i++)
+                {
+                    currentFactorialNumber *= i;
+                }
+
+                sumFactorial += currentFactorialNumber;
+            }
+
+            if (sumFactorial == num)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
