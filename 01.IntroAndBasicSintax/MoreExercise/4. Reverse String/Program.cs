60e947f48155431e1b8e824230197c74ef9a524e
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-09 08:39:34 +0300

    Add Reverse string

---
+++ 01.IntroAndBasicSintax/MoreExercise/4. Reverse String/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _004._Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string item = Console.ReadLine();
+            string meti = new string(item.ToCharArray().Reverse().ToArray());
+
+            Console.WriteLine(meti);
+        }
+    }
+}
