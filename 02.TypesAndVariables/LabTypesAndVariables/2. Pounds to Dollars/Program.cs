1498f5f02324307342b06ebc0658ea8acda40e84
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-11 23:39:19 +0300

    Add Pound to dollars

---
+++ 02.TypesAndVariables/LabTypesAndVariables/2. Pounds to Dollars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02._Pounds_to_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal dollar = decimal.Parse(Console.ReadLine());
+
+            decimal pound = dollar * 1.31m;
+
+            Console.WriteLine($"{pound:f3}");
+        }
+    }
+}
