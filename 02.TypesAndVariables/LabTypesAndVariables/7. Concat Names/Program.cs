e8544d9751a4db20c18cb49ccecdc92ff755bfc9
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-11 23:43:56 +0300

    Add Concat names

---
+++ 02.TypesAndVariables/LabTypesAndVariables/7. Concat Names/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _07._Concat_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string nameLast = Console.ReadLine();
+            string specialSymbols = Console.ReadLine();
+
+            Console.WriteLine($"{name}{specialSymbols}{nameLast}");
+        }
+    }
+}
