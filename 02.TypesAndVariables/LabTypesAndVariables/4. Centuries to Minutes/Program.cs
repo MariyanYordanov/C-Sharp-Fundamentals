908ef6605068dc9683f01df8c9883fc7da38d15d
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-11 23:41:08 +0300

    Add Centuries

---
+++ 02.TypesAndVariables/LabTypesAndVariables/4. Centuries to Minutes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04._Centuries_to_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte centures = byte.Parse(Console.ReadLine());
+
+            ushort years = (ushort)(centures * 100);
+            int days = (int)(years * 365.2422);
+            int hours = (int)(days * 24);
+            ulong minutes = (ulong)(hours * 60);
+
+            Console.WriteLine($"{centures} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
