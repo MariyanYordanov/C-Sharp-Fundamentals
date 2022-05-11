339d50bcd1e9b3501417a0c7558ee99046e299a4
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-11 23:42:04 +0300

    Add Special numbers

---
+++ 02.TypesAndVariables/LabTypesAndVariables/5. Special Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool specialNumber;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int sum = 0;
+                int last = i;
+                while (last > 0)
+                {
+                    sum += last % 10;
+                    last /= 10;
+                }
+
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    specialNumber = true;
+                }
+                else
+                {
+                    specialNumber = false;
+                }
+
+                Console.WriteLine($"{i} -> {specialNumber}");
+            }
+        }
+    }
+}
+
