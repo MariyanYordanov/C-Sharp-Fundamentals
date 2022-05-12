0518ab3d9447765a74a49998b7a0ed390b70ee92
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-12 18:21:00 +0300

    Add HTTP

---
+++ 03.HTTP/03.0.0.HTTP Basics/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _03._0._0.HTTP_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ЧЕТЕНЕ ОТ КОНЗОЛАТА НА ЧИСЛА И СЪЗДАВАНЕ НА МАСИВ С РАЗДЕЛИТЕЛ ПО ИЗБОР, ИЗБИРАНЕ НА СТРИНГ И ПАРСВАНЕ КЪМ ЧИСЛО, И ПРЕВРЪЩАНЕ В МАСИВ
+
+            //int[] array = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+
+
+            //проверка за симетричност на масив
+
+
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array = new int[n];
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //bool simetric = true;
+
+            //for (int i = 0; i < array.Length/2; i++)
+            //{
+            //    if (array[i] != array[n-i-1])
+            //    {
+            //        simetric = false;
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine(simetric);
+
+
+
+            // обръщане на масив
+
+            //int[] array = {11, 42, 3, 1, 51};
+            //int lenght = array.Length;
+            //int[] arrayReverse = new int[lenght];
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    arrayReverse[lenght - i - 1] = array[i];
+            //}
+
+            //for (int i = 0; i < arrayReverse.Length; i++)
+            //{
+            //    Console.Write(string.Join(",", arrayReverse[i]));
+            //    //Console.Write(arrayReverse[i] + " ");
+            //}
+
+
+            
+
+        }
+    }
+}
