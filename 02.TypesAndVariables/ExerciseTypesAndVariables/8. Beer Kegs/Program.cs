58c9ca94c3acf0352c932de0174fe7a3759a9331
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-13 17:17:44 +0300

    Add beer kegs

---
+++ 02.TypesAndVariables/ExerciseTypesAndVariables/8. Beer Kegs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double biggerKeg = double.MinValue;
+            string firstModel = string.Empty;
+
+            for (int i = 1; i <= n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double volume = (double)(Math.PI * Math.Pow(radius,2) * height);
+
+                if (volume >= biggerKeg)
+                {
+                    biggerKeg = volume;
+                    firstModel = model;
+                }
+            }
+            Console.WriteLine(firstModel);
+        }
+    }
+}
