dcbfc818d220f7f769d0c2f36897372907191bf1
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-13 17:15:42 +0300

    Add Triple

---
+++ 02.TypesAndVariables/ExerciseTypesAndVariables/6. Triples of Latin Letters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06._Triples_of_Latin_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int consantNumber = 97;
+            for (int i = consantNumber; i < number + consantNumber; i++)
+            {
+                for (int j = consantNumber; j < number + consantNumber; j++)
+                {
+                    for (int k = consantNumber; k < number + consantNumber; k++)
+                    {
+                        Console.WriteLine($"{(char)i}{(char)j}{(char)k}");
+                    }
+                }
+            }
+        }
+    }
+}
