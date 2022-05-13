b48235cd82286119f33ad398f68e52101b4b39a1
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-13 17:20:38 +0300

    Add snowball

---
+++ 02.TypesAndVariables/ExerciseTypesAndVariables/11. Snowballs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+namespace _11._Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger snowballSnow = 0;
+            BigInteger snowballTime = 0;
+            int snowballQuality = 0;
+            
+            BigInteger snowballValue = int.MinValue;
+
+            for (int i = 1; i <= n; i++)
+            {
+                BigInteger snow = BigInteger.Parse(Console.ReadLine());
+                BigInteger time = BigInteger.Parse(Console.ReadLine());
+                int quality = int.Parse(Console.ReadLine());
+                BigInteger result = snow / time;
+                BigInteger value = BigInteger.Pow(result, quality);
+
+                if (value >= snowballValue)
+                {
+                    snowballValue = value;
+                    snowballSnow = snow;
+                    snowballTime = time;
+                    snowballQuality = quality;
+                }
+            }
+            Console.WriteLine($"{snowballSnow} : {snowballTime} = {snowballValue} ({snowballQuality})");
+        }
+    }
+}
