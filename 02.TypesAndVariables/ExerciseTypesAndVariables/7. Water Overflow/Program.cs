bdb1ed4210071981395774ea6892da8496e9d64e
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-13 17:16:35 +0300

    Add Water overflow

---
+++ 02.TypesAndVariables/ExerciseTypesAndVariables/7. Water Overflow/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int litera = int.Parse(Console.ReadLine());
+
+                if ((sum + litera) <= 255)
+                {
+                    sum += litera;
+                }
+                else 
+                {
+                    Console.WriteLine($"Insufficient capacity!");
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
