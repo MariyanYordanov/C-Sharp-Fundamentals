f9ab66b0a323dcb710ac064d1b79d626456dbf70
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-13 17:19:47 +0300

    Add pokemon

---
+++ 02.TypesAndVariables/ExerciseTypesAndVariables/10. Poke Mon/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _10._Poke_Mon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+
+            int countPoke = 0;
+            int halfPower = (int)(power * 0.5);
+
+            while (power >= distance)
+            {
+                power -= distance;
+                countPoke++;
+
+                if (power == halfPower && exhaustionFactor > 0)
+                {
+                    power = (int)(power / exhaustionFactor);
+                }
+            }
+            Console.WriteLine(power);
+            Console.WriteLine(countPoke);
+        }
+    }
+}
