b18145c29de2a085c3b696effc0604386ed810b2
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-13 17:19:01 +0300

    Add Spice

---
+++ 02.TypesAndVariables/ExerciseTypesAndVariables/9. Spice Must Flow/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int spice = int.Parse(Console.ReadLine());
+            
+            int count = 0;
+            int powerSum = 0;
+            while (spice >= 100)
+            {
+                int power = spice;
+                power -= 26;
+                powerSum += power;
+                count++;
+                spice -= 10;
+                if (spice < 100)
+                {
+                    powerSum -= 26;
+                    break;
+                }
+            }
+            Console.WriteLine($"{count}");
+            Console.WriteLine($"{powerSum}");
+            
+        }
+    }
+}
