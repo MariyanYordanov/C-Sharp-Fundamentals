45f50fa9064823d76912735ad54830b4cc1d8171
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-14 18:45:36 +0300

    Add Floating equality

---
+++ 02.TypesAndVariables/MoreExercise/3. Floating Equality/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03._Floating_Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double first = double.Parse(Console.ReadLine());
+            double second = double.Parse(Console.ReadLine());
+
+            bool eps = false;
+            double diff = Math.Abs(first - second);
+            if (diff >= 0.000001)
+            {
+                eps = false;
+            }
+            else
+            {
+                eps = true;
+            }
+            Console.WriteLine(eps);
+        }
+    }
+}
