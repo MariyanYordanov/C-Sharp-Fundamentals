3317ba682c66d15ab3e8e6a0045a28efe3bb5f6c
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-14 18:44:37 +0300

    Add From left to right

---
+++ 02.TypesAndVariables/MoreExercise/2. From Left to The Right/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace _02._From_Left_to_The_Right
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                BigInteger[] numbers = Console.ReadLine().Split(" ").Select(BigInteger.Parse).ToArray();
+                BigInteger sum = 0;
+                int digit = 0;
+                if (numbers[0] >= numbers[1])
+                {
+                    while (numbers[0] != 0)
+                    {
+                        digit = (int)Math.Abs((decimal)numbers[0] % 10);
+                        numbers[0] /= 10;
+                        sum += digit;
+                    }
+                }
+                else
+                {
+                    while (numbers[1] != 0)
+                    {
+                        digit = (int)Math.Abs((decimal)numbers[1] % 10);
+                        numbers[1] /= 10;
+                        sum += digit;
+                    }
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
