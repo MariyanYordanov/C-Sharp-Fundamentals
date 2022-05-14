c3d73c279fb04457bf2bd07902291651758ff49d
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-14 18:51:18 +0300

    Add Balanced bracket

---
+++ 02.TypesAndVariables/MoreExercise/6. Balanced Brackets/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _06._Balanced_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                string bracket = Console.ReadLine();
+                
+                if (bracket == "(")
+                {
+                    count++;
+                }
+                if (bracket == ")")
+                {
+                    count--;
+                }
+                if (count < 0)
+                {
+                    Console.WriteLine("UNBALANCED");
+                    return;
+                }
+            }
+
+            if (count == 0)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
