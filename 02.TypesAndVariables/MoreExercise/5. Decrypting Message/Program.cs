51095e4ad0f32b27b4273f74793839a67234b329
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-14 18:49:08 +0300

    Add message

---
+++ 02.TypesAndVariables/MoreExercise/5. Decrypting Message/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05._Decrypting_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int lines = int.Parse(Console.ReadLine());
+
+            char[] word = new char[lines];
+
+            for (int i = 0; i < lines; i++)
+            {
+                char letterChar = char.Parse(Console.ReadLine());
+                int letterInt = (int)letterChar + key;
+                char letter = (char)letterInt;
+                word[i] = letter;
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
