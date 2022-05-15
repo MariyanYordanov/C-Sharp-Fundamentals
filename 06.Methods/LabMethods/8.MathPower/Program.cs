40d30eebf3388ac8d673d4916162dace172f5381
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-15 22:35:56 +0300

    Add Math power

---
+++ 06.Methods/LabMethods/8.MathPower/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _08.MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Power(n, power));
+
+        }
+        private static double Power(double n, double power)
+        {
+            double result = 1d;
+            for (int i = 0; i < power; i++)
+            {
+                result *= n;
+            }
+            return result;
+        }
+    }
+}
