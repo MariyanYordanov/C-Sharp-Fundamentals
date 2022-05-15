39a8a49dca361d9e08b04fa41cf8372846a43821
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-15 22:34:39 +0300

    Add Repeart string

---
+++ 06.Methods/LabMethods/7.RepeatString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07.RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            RepeatString(input, n);
+        }
+
+        static string RepeatString(string input, int n)
+        {
+            string result = "";
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(input);
+            }
+            return result;
+        }
+    }
+}
