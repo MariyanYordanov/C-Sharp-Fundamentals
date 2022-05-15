31f44b6d6287bd90cc880e75a04019f0c8627710
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-15 22:27:49 +0300

    Create Methods

---
+++ 06.Methods/LabMethods/1.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _5.Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            EvenOrOddNumber(n);
+        }
+
+        static void EvenOrOddNumber (int a)
+        {
+           
+            if (a > 0)
+            {
+                Console.WriteLine($"The number {a} is positive.");
+            }
+            else if (a < 0)
+            {
+                Console.WriteLine($"The number {a} is negative.");
+            }
+            else 
+            {
+                Console.WriteLine($"The number {a} is zero.");
+                
+            }
+        }
+    }
+}
