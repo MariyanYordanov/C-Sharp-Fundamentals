ba944bd4c4c6eacda3b1a7e06f9d5acf3c5e01be
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-22 22:44:34 +0300

    Add Characters in Range

---
+++ 06.Methods/ExerciseMethods/3.CharactersInRange/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbolFirst = char.Parse(Console.ReadLine());
+            char symbolSecond = char.Parse(Console.ReadLine());
+            PrintAllSymbolsBetween(symbolFirst, symbolSecond);
+        }
+
+        private static void PrintAllSymbolsBetween(char symbolFirst, char symbolSecond)
+        {
+            char temp = symbolFirst;
+            char temp2 = symbolSecond;
+            if (symbolFirst < symbolSecond)
+            {
+                symbolSecond = temp;
+                symbolFirst = temp2;
+                for (char i = (char)(temp + 1); i < temp2; i++)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            else
+            {
+                for (char i = (char)(symbolSecond + 1); i < symbolFirst; i++)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            
+        }
+
+        // решението на Митко Петобоя
+
+        //private static void PrintAllSymbolsBetween(char symbolFirst, char symbolSecond)
+        //{
+        //    int startChar = Math.Min(symbolFirst, symbolSecond);
+        //    int endChar = Math.Max(symbolFirst, symbolSecond);
+
+        //    for (int i = startChar + 1; i < endChar; i++)
+        //    {
+        //        Console.WriteLine((char)i + " ");
+        //    }
+        //}
+
+    }
+}
