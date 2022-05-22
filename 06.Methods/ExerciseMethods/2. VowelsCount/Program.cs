b52ebada772a883c06e0da8d0b9ec40445571fa0
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-22 22:43:34 +0300

    Add Vowels

---
+++ 06.Methods/ExerciseMethods/2. VowelsCount/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();
+            Console.WriteLine(VowelsCount(text));
+        }
+
+        private static int VowelsCount(string text)
+        {
+            int count = 0;
+            foreach (char vowels in text)
+            {
+                if ("aeiou".Contains(vowels))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        // решение на един ред 
+
+        //private static int VowelsCount(string text)
+        //{
+        //    Console.WriteLine(text.Count(vowles => "aeiou".Contains(vowles)));
+        //}
+    }
+}
