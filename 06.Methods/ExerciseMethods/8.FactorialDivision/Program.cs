e0130d9ba0889b2f5035ef625b61ba72ec3333d6
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-22 22:49:30 +0300

    Add Factorial Division

---
+++ 06.Methods/ExerciseMethods/8.FactorialDivision/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08.FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{FactorialDivision(firstNumber, secondNumber):f2}");
+        }
+
+        private static double FactorialDivision(double firstNumber, double secondNumber)
+        {
+            return FactorialFromNumber(firstNumber) / FactorialFromNumber(secondNumber);
+        }
+
+        private static double FactorialFromNumber(double number)
+        {
+            double result = 1;
+            for (int i = 1; i < number ; i++)
+            {
+                result *= (i + 1);
+            }
+            return result;
+        }
+    }
+}
