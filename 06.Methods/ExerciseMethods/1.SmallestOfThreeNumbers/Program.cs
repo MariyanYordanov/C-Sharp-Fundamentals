9b5eb91f05e8b9e68ffeb02c0b39dbd07cbaa636
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-22 22:41:23 +0300

    Add Smallest of Three Numbers

---
+++ 06.Methods/ExerciseMethods/1.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SmallestOfThreeNumbers(num1, num2, num3));
+        }
+
+        private static int SmallestOfThreeNumbers(int num1, int num2, int num3)
+        {
+            int result = Math.Min(num1, Math.Min(num2, num3));
+            return result;
+        }
+    }
+}
