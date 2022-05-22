895747a8581e3939d83840483e666f5d8c71c9ca
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-22 22:47:37 +0300

    Add Exercise Methods

---
+++ 06.Methods/ExerciseMethods/6.MiddleCharacters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _006.MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            FindMiddleCharacter(input);
+        }
+        
+        private static void FindMiddleCharacter(string input)
+        {
+            if (input.Length % 2 == 0)
+            {
+                Console.Write(input[(input.Length - 1) / 2]);
+                Console.Write(input[((input.Length - 1)/ 2) + 1]);
+            }
+            else
+            {
+                Console.WriteLine(input[input.Length / 2]);
+            }
+            
+        }
+    }
+}
