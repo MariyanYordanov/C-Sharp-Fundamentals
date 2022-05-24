3fb69279c808978cafe98f42d64995b9040515b8
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-24 19:47:10 +0300

    Add Longer Line

---
+++ 06.Methods/MoreExercise/3.LongerLine/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _3.LongerLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            CloserToCenterPoint(x1, y1, x2, y2);
+        }
+
+
+        private static void CloserToCenterPoint(double x1, double y1, double x2, double y2)
+        {
+            if ((Math.Sqrt((x1 * x1) + (y1 * y1))) > Math.Sqrt(((x2 * x2) + (y2 * y2))))
+            {
+
+                Console.WriteLine($"({x2}, {y2})");
+            }
+            else if ((Math.Sqrt((x1 * x1) + (y1 * y1))) < Math.Sqrt(((x2 * x2) + (y2 * y2))))
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+        }
+    }
+}
