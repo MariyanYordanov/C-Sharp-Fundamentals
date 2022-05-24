3e6517fca88fc066eeecae9748786dba87912722
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-24 19:48:13 +0300

    Add Tribonacci Sequence

---
+++ 06.Methods/MoreExercise/4.TribonacciSequence/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _4.TribonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n == 1)
+            {
+                Console.Write(1 + " ");
+            }
+            else if (n == 2)
+            {
+                Console.Write(1 + " ");
+                Console.Write(1 + " ");
+            }
+            else if (n == 3)
+            {
+                Console.Write(1 + " ");
+                Console.Write(1 + " ");
+                Console.Write(2 + " ");
+            }
+            else
+            {
+                TribonachiSequences(n);
+            }
+            
+        }
+
+        private static void TribonachiSequences(int n)
+        {
+            int[] fibonachiSequences = new int[n];
+
+            fibonachiSequences[0] = 1;
+            fibonachiSequences[1] = 1;
+            fibonachiSequences[2] = 2;
+            
+            Console.Write(fibonachiSequences[0] + " ");
+            Console.Write(fibonachiSequences[1] + " ");
+            Console.Write(fibonachiSequences[2] + " ");
+
+            for (int i = 3; i < fibonachiSequences.Length; i++)
+            {
+                fibonachiSequences[i] = fibonachiSequences[i - 1] + fibonachiSequences[i - 2] + fibonachiSequences[i - 3];
+                Console.Write(fibonachiSequences[i] + " ");
+            }
+        }
+    }
+}
