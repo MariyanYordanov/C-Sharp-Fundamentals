5b6fff9ee964fa594ce8c4dac37a5a240e5c11d3
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-26 22:25:28 +0300

    Add task

---
+++ 07.BitwiseOperations/Lab.04.BitDestroyer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab._04.BitDestroyer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            int mask = ~(1 << p);
+            int NewNumber = n & mask;
+            Console.WriteLine(NewNumber);
+        }
+    }
+}
