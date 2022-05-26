189f4368fb7c42a2a3aa60ebcaf3c7b4edc5bce7
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-26 22:24:34 +0300

    Add task

---
+++ 07.BitwiseOperations/Lab.02.BitAtPosition1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab._02.BitAtPosition1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            num >>= 1;
+            num &= 1;
+            Console.WriteLine(num);
+        }
+    }
+}
