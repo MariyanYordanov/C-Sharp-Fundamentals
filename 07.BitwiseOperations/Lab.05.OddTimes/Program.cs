deaac7f55d994093fa63edd3454e9d3572efae92
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-26 22:26:19 +0300

    Add task

---
+++ 07.BitwiseOperations/Lab.05.OddTimes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab._05.OddTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result ^= arr[i];
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
