3ee4ae9adddb0a67e92a215b6ef6621b7885e074
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-26 22:27:00 +0300

    Add task

---
+++ 07.BitwiseOperations/Lab.6. Tri-bitSwitch/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab._6._Tri_bitSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            int mask = 7 << p;
+            int result = number ^ mask;
+            Console.WriteLine(result);
+        }
+    }
+}
