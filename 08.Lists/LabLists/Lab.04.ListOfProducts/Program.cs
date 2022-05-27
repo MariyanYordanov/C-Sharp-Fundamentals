19113cab3cde683a146672fdc7c045596ba42158
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-27 20:35:58 +0300

    Add task

---
+++ 08.Lists/LabLists/Lab.04.ListOfProducts/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Lab._04.ListOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> orderProducts = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string product = Console.ReadLine();
+                orderProducts.Add(product);
+            }
+
+            orderProducts.Sort();
+            
+            for (int i = 0; i < orderProducts.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{orderProducts[i]}");
+            }
+            
+        }
+    }
+}
