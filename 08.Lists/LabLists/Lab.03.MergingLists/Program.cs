266ab6f70a91b090f501850639258ea18f67a23c
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-27 20:35:11 +0300

    Add task

---
+++ 08.Lists/LabLists/Lab.03.MergingLists/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Lab._03.MergingLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOne = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> listTwo = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int N = 0;
+            if (listOne.Count > listTwo.Count)
+            {
+                N = listOne.Count;
+            }
+            else
+            {
+                N = listTwo.Count;
+            }
+            List<int> result = new List<int>();
+            for (int i = 0; i < N; i++)
+            {
+                if (i < listOne.Count)
+                {
+                    result.Add(listOne[i]);
+                }
+                if (i < listTwo.Count)
+                {
+                    result.Add(listTwo[i]);
+                }
+                
+            }
+
+            Console.WriteLine(string.Join(" ",result));
+        }
+    }
+}
