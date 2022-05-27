f8f99f759a8a39e0ee9333c2c0e83d4c8d60bf51
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-27 20:34:39 +0300

    Add task

---
+++ 08.Lists/LabLists/Lab.02.Gauss'Trick/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Lab._02.Gauss_Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            
+            for (int i = 0; i < list.Count / 2; i++)
+            {
+                int sum = list[i] + list[list.Count - 1 - i];
+                Console.Write(sum + " ");
+            }
+
+            if (list.Count % 2 != 0)
+            {
+                Console.WriteLine(list[list.Count / 2]);
+            }
+        }
+    }
+}
