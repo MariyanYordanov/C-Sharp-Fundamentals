a39284be41a04d4abbcf6cf67b6e0da2e2347e76
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-28 20:31:32 +0300

    Add Change List

---
+++ 08.Lists/Exerciselists/E.02.ChangeList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._02.ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string[] command = Console.ReadLine().Split();
+
+            while (command[0] != "end")
+            {
+                int element = int.Parse(command[1]);
+                switch (command[0])
+                {
+                    case "Delete":
+                        list.RemoveAll(item => item == element);
+                        break;
+                    case "Insert":
+                        int position = int.Parse(command[2]);
+                        list.Insert(position, element);
+                        break;
+                }
+
+                command = Console.ReadLine().Split();
+            }
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
