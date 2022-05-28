a8a19917dd5025ae2042fc89ac690dc5ff57c0a2
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-28 20:30:50 +0300

    Add Train

---
+++ 08.Lists/Exerciselists/E.01.Train/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagon = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int maxCapasity = int.Parse(Console.ReadLine());
+            string[] command = Console.ReadLine().Split();
+
+            while (command[0] != "end")
+            {
+                if (command[0] == "Add")
+                {
+                    int number = int.Parse(command[1]);
+                    wagon.Add(number);
+                }
+
+                else
+                {
+                    int number = int.Parse(command[0]);
+                    for (int i = 0; i < wagon.Count; i++)
+                    {
+                        if (wagon[i] + number <= maxCapasity)
+                        {
+                            wagon[i] = wagon[i] + number;
+                            break;
+                        }
+                    }
+                }
+
+                command = Console.ReadLine().Split();
+            }
+            foreach (var item in wagon)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
