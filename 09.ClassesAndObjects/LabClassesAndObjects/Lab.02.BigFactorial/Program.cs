66ab767b17269557128f606e2b2ac701046df44d
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-31 23:42:20 +0300

    Add task

---
+++ 09.ClassesAndObjects/LabClassesAndObjects/Lab.02.BigFactorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace Lab._02.BigFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+            for (int i = 2; i <= N; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
