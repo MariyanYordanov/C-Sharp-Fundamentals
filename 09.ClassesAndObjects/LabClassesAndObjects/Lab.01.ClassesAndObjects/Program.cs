c2db3e2632b4b570aecf00d0fe3893cf2026ca65
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-31 23:41:41 +0300

    Add Classes and Objects

---
+++ 09.ClassesAndObjects/LabClassesAndObjects/Lab.01.ClassesAndObjects/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            Random rnd = new Random();
+            
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                int swapPosition = rnd.Next(words.Length);
+                string temp = words[i];
+                words[i] = words[swapPosition];
+                words[swapPosition] = temp;
+            }
+            
+            Console.WriteLine(string.Join("\n", words));
+        }
+    }
+
+}
