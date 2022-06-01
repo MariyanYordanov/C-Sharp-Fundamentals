e9e3e6819ad7de5ef683aa39a90921fe99079760
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-01 23:15:35 +0300

    Add task

---
+++ 09.ClassesAndObjects/ExerciseClassesAndObjects/E.07.OrderByAge/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._07.OrderByAge
+{
+    class Program
+    {
+        public class Persons
+        {
+            public string Name { get; set; }
+            public string ID { get; set; }
+            public int Age { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Name} with ID: {ID} is {Age} years old.";
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Persons> person = new List<Persons>();
+            string input = Console.ReadLine();
+            while (input != "End")
+            {
+                string[] command = input.Split();
+
+                string name = command[0];
+                string id = command[1];
+                int age = int.Parse(command[2]);
+
+                Persons currentIPerson = person.FirstOrDefault(s => s.ID == id);
+                if (currentIPerson == null)
+                {
+                    person.Add(new Persons()
+                    {
+                        Name = name,
+                        ID = id,
+                        Age = age,
+                    });
+                }
+                else
+                {
+                    currentIPerson.Name = name;
+                    currentIPerson.ID = id;
+                    currentIPerson.Age = age;
+                    
+                }
+
+                input = Console.ReadLine();
+            }
+            foreach (Persons persons in person.OrderBy(x => x.Age))
+            {
+                Console.WriteLine(persons);
+            }
+        }
+    }
+}
