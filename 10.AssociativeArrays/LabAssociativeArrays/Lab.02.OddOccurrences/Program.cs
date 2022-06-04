eb7e0c7fffd8f691279723ea64340f7c70c9f2bc
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-04 21:53:42 +0300

    Add task

---
+++ 10.AssociativeArrays/LabAssociativeArrays/Lab.02.OddOccurrences/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab._02.OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+            foreach(string word in words)
+            {
+                string wordCaseSensitive = word.ToLower();
+                if (counts.ContainsKey(wordCaseSensitive))
+                {
+                    counts[wordCaseSensitive]++;
+                }
+                else
+                {
+                    counts.Add(wordCaseSensitive, 1);
+                }
+                
+            }
+            foreach (var count in counts)
+            {
+                if (count.Value % 2 != 0)
+                {
+                    Console.Write(count.Key + " ");
+                }
+               
+            }
+            
+        }
+    }
+}
