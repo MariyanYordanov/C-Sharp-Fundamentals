acc35693abca562bacd6ddb098963f095d93d2dc
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-04 21:59:04 +0300

    Add task

---
+++ 10.AssociativeArrays/LabAssociativeArrays/Lab.05.WordFilter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab._05.WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wordsArray = Console.ReadLine().Split();
+            foreach (string word in wordsArray.Where(x => x.Length % 2 == 0))
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
