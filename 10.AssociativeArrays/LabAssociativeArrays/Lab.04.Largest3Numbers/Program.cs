6838f7cf64fa4d4ea80b0f57309c7d8fe85c85a2
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-04 22:10:32 +0300

    Add task

---
+++ 10.AssociativeArrays/LabAssociativeArrays/Lab.04.Largest3Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // my way
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var num = numbers.OrderByDescending(x => x).Take(3).ToList();
+            Console.WriteLine(string.Join(" ",num));
+
+
+            // SoftUni way
+            int[] n = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(n => n).ToArray();
+            int count = n.Length >= 3 ? 3 : n.Length;// if (n.Length >= 3) { count = 3 } 
+            //                        ^   ^
+            //                        if  else             // else { count = n.Length }
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"{n[i]}");
+            }
+        }
+    }
+}
