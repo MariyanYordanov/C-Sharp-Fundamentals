e79014aa03d0299dd86d5f96b31dbc71b43ffc44
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-04 21:57:27 +0300

    Add task

---
+++ 10.AssociativeArrays/LabAssociativeArrays/Lab.03.WordSynonyms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab._03.WordSynonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> WORDS = new Dictionary<string, List<string>>();
+            for (int i = 0; i < n; i++)
+            {
+                string wordKey = Console.ReadLine();
+                string wordValue = Console.ReadLine();
+                if (!WORDS.ContainsKey(wordKey))
+                {
+                    WORDS.Add(wordKey, new List<string>());
+                }
+
+                WORDS[wordKey].Add(wordValue);
+            }
+            foreach (var item in WORDS)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
