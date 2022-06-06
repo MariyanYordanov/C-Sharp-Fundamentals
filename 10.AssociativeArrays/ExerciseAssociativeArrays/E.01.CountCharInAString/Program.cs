db7aeeb39ba7d024219ba6b0c0a85e45c2258080
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-06 04:09:19 +0300

    Add task

---
+++ 10.AssociativeArrays/ExerciseAssociativeArrays/E.01.CountCharInAString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E._01.CountCharInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] letters = Console.ReadLine().ToCharArray();
+            Dictionary<char, int> textList = new Dictionary<char, int>();
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (letters[i] != ' ')
+                {
+                    if (!textList.ContainsKey(letters[i]))
+                    {
+                        textList.Add(letters[i], 0);
+                    }
+                    textList[letters[i]]++;
+                }
+                
+            }
+            
+            foreach (var letter in textList)
+            {
+                Console.WriteLine($"{letter.Key} -> {letter.Value}");
+            }
+        }
+    }
+}
