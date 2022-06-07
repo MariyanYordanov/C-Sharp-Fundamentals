f26554b925d5bacfc7594b1b543fbaf275bd941a
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-07 22:34:26 +0300

    Add task

---
+++ 10.AssociativeArrays/MoreAssociativeArrays/ME.05.DragonArmy/DragonStat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ME._05.DragonArmy
+{
+    public class DragonStat
+    {
+        public DragonStat()
+        {
+            Health = 250;
+            Damage = 45;
+            Armor = 10;
+        }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Damage { get; set; }
+        public int Armor { get; set; }
+    }
+}
---
+++ 10.AssociativeArrays/MoreAssociativeArrays/ME.05.DragonArmy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ME._05.DragonArmy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedDictionary<string, Dictionary<string, List<int>>> dragons = new SortedDictionary<string, Dictionary<string, List<int>>>();
+            int health = 250;
+            int damage = 45;
+            int armor = 10;
+
+        }
+    }
+}
