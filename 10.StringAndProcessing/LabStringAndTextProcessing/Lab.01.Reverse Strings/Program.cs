9776e1e725b4bfaed4e69bac22a8b81d43b57f65
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-09 01:18:59 +0300

    Add task

---
+++ 10.StringAndProcessing/LabStringAndTextProcessing/Lab.01.Reverse Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab._01.Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            while (word != "end")
+            {
+                string reverced = "";
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reverced += word[i];
+                }
+
+                Console.WriteLine($"{word} = {reverced}");
+                word = Console.ReadLine();
+            }
+        }
+    }
+}
