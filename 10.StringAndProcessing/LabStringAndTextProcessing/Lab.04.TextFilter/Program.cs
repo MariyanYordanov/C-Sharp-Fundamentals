a7a2081199e7d232169fd8c6ed43087a8dc5b582
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-09 01:21:01 +0300

    Add task

---
+++ 10.StringAndProcessing/LabStringAndTextProcessing/Lab.04.TextFilter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab._04.TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string input = Console.ReadLine();
+            string text = input;
+            foreach (string word in bannedWords)
+            {
+                string astarsks = new string('*', word.Length);
+                if (text.Contains(word))
+                {
+                    text = text.Replace(word, astarsks);
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
