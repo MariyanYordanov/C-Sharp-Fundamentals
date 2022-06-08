064a7aa4f50e0e25ac90f63f5698fedda6cd29e8
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-09 01:19:48 +0300

    Add task

---
+++ 10.StringAndProcessing/LabStringAndTextProcessing/Lab.02.RepeatStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+namespace Lab._02.RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            StringBuilder sb = new StringBuilder();
+            foreach (string word in arr)
+            {
+                int counter = word.Length;
+                for (int i = 0; i < counter; i++)
+                {
+                    sb.Append(word);
+                }
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
