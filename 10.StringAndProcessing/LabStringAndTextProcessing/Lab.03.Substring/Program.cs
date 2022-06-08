62deb22fa6d270ec26b24db12e1941fdf067e3e1
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-09 01:20:29 +0300

    Add task

---
+++ 10.StringAndProcessing/LabStringAndTextProcessing/Lab.03.Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab._03.Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString = Console.ReadLine();
+            string secondString = Console.ReadLine();
+            string temp = secondString;
+            int findIndex = temp.IndexOf(firstString);
+            while (findIndex >= 0)
+            {
+                string reOrganizedString = temp.Remove(findIndex, firstString.Length);
+                temp = reOrganizedString;
+                findIndex = temp.IndexOf(firstString);
+            }
+
+            Console.WriteLine(temp);
+        }
+    }
+}
