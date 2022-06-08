30ec4d567af704c3e89fd42c86a1131de40943f9
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-09 01:21:57 +0300

    Add task

---
+++ 10.StringAndProcessing/LabStringAndTextProcessing/Lab.05.Digits,LettersAndOther/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab._05.Digits_LettersAndOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char[] input = text.ToCharArray();
+            string nums = "";
+            string symbol = "";
+            string letter = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsDigit(input[i]) ==true)
+                {
+                    nums += input[i];
+                }
+                else if (Char.IsLetter(input[i]) == true)
+                {
+                    letter += input[i];
+                }
+                else
+                {
+                    symbol += input[i];
+                }
+            }
+            Console.WriteLine(nums);
+            Console.WriteLine(letter);
+            Console.WriteLine(symbol);
+        }
+    }
+}
