b672084599464bd957beb1a311c5470119fca0d0
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-11 00:21:48 +0300

    Add task

---
+++ 10.StringAndProcessing/ExercisesStringAndTextProcessing/E.06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace _06.ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder SB = new StringBuilder();
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] != input[i + 1])
+                {
+                    SB.Append(input[i]);
+                }
+
+            }
+
+            SB.Append(input[input.Length - 1]);
+
+            Console.WriteLine(SB);
+        }
+
+    }
+}
