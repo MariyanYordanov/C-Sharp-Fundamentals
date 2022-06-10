5f216575859cc33ef9428b7cbaacbc1faf909b29
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-11 00:21:13 +0300

    Add task

---
+++ 10.StringAndProcessing/ExercisesStringAndTextProcessing/E.05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace E._05.MultiplyBigNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            int reminder = 0;
+            StringBuilder stringNumber = new StringBuilder();
+            if (n == 0 || input =="0")
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                int number = int.Parse(input[i].ToString());
+                int product = n * number + reminder;
+                int result = product % 10;
+                reminder = product / 10;
+                stringNumber.Insert(0, result);
+            }
+
+            if (reminder > 0)
+            {
+                stringNumber.Insert(0, reminder);
+            }
+            
+            Console.WriteLine(stringNumber);
+        }
+
+    }
+}
