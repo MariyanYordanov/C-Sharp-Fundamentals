02dfffc79ecbaa25e0d5345799e3f89f77254e40
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-11 00:20:39 +0300

    Add task

---
+++ 10.StringAndProcessing/ExercisesStringAndTextProcessing/E.04.CaesarCipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace E._04.CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder caesarCipher = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char newChar = (char)(input[i] + 3);
+                caesarCipher.Append(newChar);
+            }
+            Console.WriteLine(caesarCipher);
+        }
+    }
+}
