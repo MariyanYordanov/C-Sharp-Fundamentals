aa3806cdf2c86bd725da475c6d72556e93d84019
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-11 00:20:05 +0300

    Add task

---
+++ 10.StringAndProcessing/ExercisesStringAndTextProcessing/E.03.ExtractFile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace E._03.ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("\\");
+            string[] fileData = input[input.Length - 1].Split(".");
+            string fileName = fileData[0];
+            string fileExtention = fileData[1];
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtention}");
+        }
+    }
+}
