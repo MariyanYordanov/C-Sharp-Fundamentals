36a346708cf2d2e3b155efacce091c01ca59b56e
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-11 00:18:50 +0300

    Add task

---
+++ 10.StringAndProcessing/ExercisesStringAndTextProcessing/E.01.ValidUsernames/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace E._01.ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ");
+            foreach (var word in input)
+            {
+                if (word.Length > 3 && word.Length <= 16)
+                {
+                    bool isLeterOrDigitOrHyphensOrUnderscore = true;
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        if (!(word[i] == '-' || word[i] == '_' || char.IsLetterOrDigit(word[i])))
+                        {
+                            isLeterOrDigitOrHyphensOrUnderscore = false;
+                            break;
+                        }
+
+                    }
+
+                    if (isLeterOrDigitOrHyphensOrUnderscore)
+                    {
+                        Console.WriteLine(word);
+                    }
+
+                }
+            }
+        }
+    }
+}
