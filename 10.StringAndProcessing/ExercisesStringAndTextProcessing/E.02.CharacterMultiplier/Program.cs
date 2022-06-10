44f04e9c8b0b5d0e67aba010598c8d2b104510ae
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-11 00:19:27 +0300

    Add task

---
+++ 10.StringAndProcessing/ExercisesStringAndTextProcessing/E.02.CharacterMultiplier/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace E._02.CharacterMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input= Console.ReadLine().Split();
+            CharacterMultiplier(input[0], input[1]);
+        }
+
+        private static void CharacterMultiplier(string firstStr, string secondStr)
+        {
+            string longer = "";
+            string smaller = "";
+            if (firstStr.Length > secondStr.Length)
+            {
+                longer = firstStr;
+                smaller = secondStr;
+            }
+            else
+            {
+                longer = secondStr;
+                smaller = firstStr;
+            }
+            string remander = longer.Substring(smaller.Length);
+            int multiplier = 0;
+            for (int i = 0; i < smaller.Length; i++)
+            {
+                multiplier += longer[i] * smaller[i];
+            }
+            foreach (var item in remander)
+            {
+                multiplier += item;
+            }
+            Console.WriteLine(multiplier);
+        }
+    }
+}
