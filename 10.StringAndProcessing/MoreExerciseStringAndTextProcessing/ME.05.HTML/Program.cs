bc7e74a1b2aa9a26ef587448ce636933fd144624
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-12 12:17:59 +0300

    Add task

---
+++ 10.StringAndProcessing/MoreExerciseStringAndTextProcessing/ME.05.HTML/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ME._05.HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string titleOfArticle = Console.ReadLine();
+            Console.WriteLine($"<h1>\n    {titleOfArticle}\n</h1>");
+            string contentOfArticle = Console.ReadLine();
+            Console.WriteLine($"<article>\n    {contentOfArticle}\n</article>");
+            string commentsAboutArticle = Console.ReadLine();
+            while (commentsAboutArticle != "end of comments")
+            {
+                Console.WriteLine($"<div>\n    {commentsAboutArticle}\n</div>");
+                commentsAboutArticle = Console.ReadLine();
+            }
+        }
+    }
+}
