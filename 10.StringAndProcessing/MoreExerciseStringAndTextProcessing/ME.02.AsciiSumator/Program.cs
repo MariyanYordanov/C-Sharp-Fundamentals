fe3fee85f0dfb36a3cbd3478dfaacab1d75c298a
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-12 12:15:25 +0300

    Add task

---
+++ 10.StringAndProcessing/MoreExerciseStringAndTextProcessing/ME.02.AsciiSumator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ME._02.AsciiSumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            int startIndex = Math.Min(first, second);
+            int endIndex = Math.Max(first, second);
+            string input = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] > startIndex && input[i] < endIndex)
+                {
+                    sum += input[i];
+                }
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
