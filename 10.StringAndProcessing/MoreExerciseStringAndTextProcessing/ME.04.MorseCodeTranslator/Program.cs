8cbfbeab5674cf3706eee40ff55ac2abc0f15a83
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-12 12:16:34 +0300

    Add task

---
+++ 10.StringAndProcessing/MoreExerciseStringAndTextProcessing/ME.04.MorseCodeTranslator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ME._04.MorseCodeTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            Dictionary<string, char> morseCode = new Dictionary<string, char>()
+            {
+                { "|", ' ' },
+                {".-", 'A'},
+                {"-...", 'B'},
+                {"-.-.", 'C'},
+                {"-..", 'D'},
+                {".", 'E'},
+                {"..-.", 'F'},
+                {"--.", 'G'},
+                {"....", 'H'},
+                {"..", 'I'},
+                {".---", 'J'},
+                {"-.-", 'K'},
+                {".-..", 'L'},
+                {"--", 'M'},
+                {"-.", 'N'},
+                {"---", 'O'},
+                {".--.", 'P'},
+                {"--.-", 'Q'},
+                {".-.", 'R'},
+                {"...", 'S'},
+                {"-", 'T'},
+                {"..-", 'U'},
+                {"...-", 'V'},
+                {".--", 'W'},
+                {"-..-", 'X'},
+                {"-.--", 'Y'},
+                {"--..", 'Z'},
+                
+            };
+            string[] letter = message.Split();
+            for (int i = 0; i < letter.Length; i++)
+            {
+                foreach (var item in morseCode)
+                {
+                    if (item.Key == letter[i])
+                    {
+                        Console.Write($"{item.Value}");
+                        break;
+                    }
+                }
+                
+            }
+
+        }
+    }
+}
