d5aeceed001c2e5e0392c6f64e8b96509da664eb
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-13 16:26:27 +0300

    Add task

---
+++ 11.RegularExpressiones/LabRegEx/Lab.02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Lab._02.MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359(?<sep>[ -])2(\k<sep>)\d{3}(\k<sep>)\d{4}\b";
+            string input = Console.ReadLine();
+            MatchCollection matchCollection = Regex.Matches(input,pattern);
+            matchCollection.Cast<Match>().Select(x => x.Value.Trim()).ToArray();
+            Console.WriteLine(string.Join(", ",matchCollection));
+        }
+    }
+}
