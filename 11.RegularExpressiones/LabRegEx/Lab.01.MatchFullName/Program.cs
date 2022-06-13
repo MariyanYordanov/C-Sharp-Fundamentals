1607702a4312614a901dd1660cdcbc19a5aa955d
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-13 16:25:52 +0300

    Add task

---
+++ 11.RegularExpressiones/LabRegEx/Lab.01.MatchFullName/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Lab._01.MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string names = Console.ReadLine();
+            Regex regex = new Regex(@"\b[A-Z][a-z]+ [A-Z][a-z]+");
+            MatchCollection matchedNames = regex.Matches(names);
+            foreach (Match name in matchedNames)
+            {
+                Console.Write($"{name.Value}" );
+            }
+            Console.WriteLine();
+        }
+    }
+}
