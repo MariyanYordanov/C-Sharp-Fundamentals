078f2a2f4b8cfba45b64f2c49210a1b889e03523
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-20 23:14:59 +0300

    Add task

---
+++ 11.RegularExpressiones/ExerciseRegEx/E.05.NetherRealms/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace E._05.NetherRealms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex damagePattern = new Regex(@"-?\d+\.?\d*");
+            Regex healthPattern = new Regex(@"[^\+\-\*\/\.\,0-9 ]");
+            Regex signsPattern = new Regex(@"[\/\*]");
+            string splitPattern = @"[,\s]+";
+            string input = Console.ReadLine();
+            string[] demons = Regex.Split(input, splitPattern).OrderBy(x => x).ToArray();
+            foreach (var demon in demons)
+            {
+                string demonName = demon;
+                MatchCollection damageCollection = damagePattern.Matches(demon);
+                double damagePoints = 0;
+                foreach (Match damage in damageCollection)
+                {
+                    double digit = double.Parse(damage.Value);
+                    damagePoints += digit;
+                }
+
+                MatchCollection healthCollection = healthPattern.Matches(demon);
+                double healthPoints = 0;
+                foreach (Match health in healthCollection)
+                {
+                    char points = char.Parse(health.Value);
+                    healthPoints += points;
+                }
+
+                MatchCollection signsCollection = signsPattern.Matches(demon);
+                foreach (Match signs in signsCollection)
+                {
+                    string sign = signs.Value;
+                    if (damagePoints > 0)
+                    {
+                        if (sign == "/")
+                        {
+                            damagePoints /= 2;
+                        }
+                        else if (sign == "*")
+                        {
+                            damagePoints *= 2;
+                        }
+                    }
+
+                }
+
+                Console.WriteLine($"{demonName} - {healthPoints} health, {damagePoints:f2} damage");
+            }
+        }
+    }
+}
