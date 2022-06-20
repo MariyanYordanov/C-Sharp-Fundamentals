fd3befd40c3565523140e4370f4be8878e2de2c6
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-20 23:12:23 +0300

    Add RegEx

---
+++ 11.RegularExpressiones/ExerciseRegEx/E.01.Furniture/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace E._01.Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @">>(?<product>[A-Za-z\s]+)<<(?<price>\d+(.\d+)?)!(?<quantity>\d+)";
+            List<string> furniture = new List<string>();
+            double total = 0;
+            while ( input != "Purchase")
+            {
+                Match matches = Regex.Match(input,pattern,RegexOptions.IgnoreCase);
+                if (matches.Success)
+                {
+                    string name = matches.Groups["product"].Value;
+                    double price = double.Parse(matches.Groups["price"].Value);
+                    int quantity = int.Parse(matches.Groups["quantity"].Value);
+                    furniture.Add(name);
+                    total += price * quantity;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Bought furniture:");
+            furniture.ForEach(Console.WriteLine);
+            Console.WriteLine($"Total money spend: {total:f2}");
+        }
+    }
+}
