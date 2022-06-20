56be6d14955c39a6e7eb2853d043e9a5f2e68913
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-20 23:15:38 +0300

    Add task

---
+++ 11.RegularExpressiones/ExerciseRegEx/E.06.ExtractEmails/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace E._06.ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\s)([a-zA-Z0-9][\w\-\.]+)+@([a-zA-Z0-9][\w\-\.]+\.[a-zA-Z0-9][\w\-]+)";
+            string input = Console.ReadLine();
+            MatchCollection matchCollection = Regex.Matches(input,pattern);
+            foreach (Match match in matchCollection)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
