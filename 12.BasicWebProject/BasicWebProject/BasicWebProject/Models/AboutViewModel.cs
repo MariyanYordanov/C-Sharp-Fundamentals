b34fd298e01f2a5c8ad0084e03f50c54f152b915
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-21 22:32:35 +0300

    Add BasicWebProject

---
+++ 12.BasicWebProject/BasicWebProject/BasicWebProject/Models/AboutViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicWebProject.Models
+{
+    public class AboutViewModel
+    {
+        public string Info { get; set; }
+        public string Description { get; set; }
+
+    }
+
+    public class Figure
+    {
+        public string Name  { get; set; }
+
+        public int Row { get; set; }
+
+        public string Column { get; set; }
+
+        public void Position(int row, string column)
+        {
+            
+        }
+    }
+}
